fdfdc9ea425312854f7def3f2afe82e7aa988030
HanSaloZu <conswintcomin@gmail.com>
2022-11-08 00:18:45 +0300

    Add City model

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+
+    class Meta:
+        verbose_name = "city"
+        verbose_name_plural = "cities"
+        db_table = "cities"
+
+    def __str__(self):
+        return f"{self.name} city"
