2c0df66a7a2413ac2cdd01c86a5a82dbd931fe65
HanSaloZu <conswintcomin@gmail.com>
2022-11-10 00:01:53 +0300

    Add GetCityGeodataTestCase to test get_city_geodata function

--- cities/tests/test_services.py
+++ cities/tests/test_services.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from rest_framework.exceptions import APIException
 
 from cities.models import City
-from cities.services import create_city, find_closest_city
+from cities.services import create_city, find_closest_city, get_city_geodata
+from utils.tests import SleepyTestCase
 
 
 class FindClosestCityTestCase(TestCase):
@@ -18,3 +20,21 @@ class FindClosestCityTestCase(TestCase):
         closest_city = find_closest_city(City.objects.all(), latitude=50, longitude=0)
 
         self.assertIsNone(closest_city)
+
+
+class GetCityGeodataTestCase(SleepyTestCase):
+    def test_get_geodata_with_correct_city_name(self):
+        geodata = get_city_geodata("Moscow")
+
+        self.assertIn("name", geodata)
+        self.assertIn("latitude", geodata)
+        self.assertIn("longitude", geodata)
+        self.assertEqual(len(geodata), 3)
+        self.assertEqual(geodata["name"], "Moscow")
+
+    def test_get_geodata_with_incorrect_city_name(self):
+        try:
+            get_city_geodata("Amogus")
+            self.fail("APIException not raised by get_city_geodata")
+        except APIException as e:
+            self.assertEqual(e.detail, "Unable to geocode")
