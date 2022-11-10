00015619de68b49f4f8ce7fc7ef57572c74e63b8
HanSaloZu <conswintcomin@gmail.com>
2022-11-10 12:06:08 +0300

    Add FindNClosestCitiesTestCase
    This TestCase tests find_n_closest_cities function

--- cities/tests/test_services.py
+++ cities/tests/test_services.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from rest_framework.exceptions import APIException
 
 from cities.models import City
-from cities.services import create_city, find_closest_city, get_city_geodata
+from cities.services import (create_city, find_closest_city,
+                             find_n_closest_cities, get_city_geodata)
 from utils.tests import SleepyTestCase
 
 
@@ -20,6 +21,48 @@ class FindClosestCityTestCase(TestCase):
         closest_city = find_closest_city(City.objects.all(), latitude=50, longitude=0)
 
         self.assertIsNone(closest_city)
+
+
+class FindNClosestCitiesTestCase(TestCase):
+    def test_find_two_closest_of_three_cities(self):
+        moscow = create_city("Moscow", 55.7504461, 37.6174943)
+        london = create_city("London", 51.5073219, -0.1276474)
+        create_city("Tokyo", 35.6828387, 139.7594549)
+
+        closest_cities = find_n_closest_cities(
+            2, City.objects.all(), latitude=50, longitude=0
+        )
+
+        self.assertIn(moscow, closest_cities)
+        self.assertIn(london, closest_cities)
+        self.assertEqual(len(closest_cities), 2)
+
+    def test_find_two_closest_of_none_city(self):
+        closest_cities = find_n_closest_cities(
+            2, City.objects.all(), latitude=50, longitude=0
+        )
+
+        self.assertEqual(len(closest_cities), 0)
+
+    def test_find_two_closest_of_one_city(self):
+        create_city("London", 51.5073219, -0.1276474)
+        closest_cities = find_n_closest_cities(
+            2, City.objects.all(), latitude=50, longitude=0
+        )
+
+        self.assertEqual(len(closest_cities), 1)
+        self.assertEqual(closest_cities[0].name, "London")
+
+    def test_find_two_closest_of_two_cities(self):
+        moscow = create_city("Moscow", 55.7504461, 37.6174943)
+        tokyo = create_city("Tokyo", 35.6828387, 139.7594549)
+        closest_cities = find_n_closest_cities(
+            2, City.objects.all(), latitude=50, longitude=0
+        )
+
+        self.assertEqual(len(closest_cities), 2)
+        self.assertIn(moscow, closest_cities)
+        self.assertIn(tokyo, closest_cities)
 
 
 class GetCityGeodataTestCase(SleepyTestCase):
