61fbd19db7aa8345f76bceac08236b66fb15e6d7
HanSaloZu <conswintcomin@gmail.com>
2022-11-10 19:08:59 +0300

    Add CitySerializer

--- cities/serializers.py
+++ cities/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 
+from cities.models import City
+from cities.services import create_city, get_city_geodata
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("id", "name", "latitude", "longitude")
+        read_only_fields = ("id", "latitude", "longitude")
+
+    def validate(self, data):
+        if (
+            self.geodata.get("name", None)
+            and City.objects.filter(name__exact=self.geodata["name"]).exists()
+        ):
+            raise serializers.ValidationError(
+                {"city": "city with this name already exists."}
+            )
+
+        return data
+
+    def extend_with_geodata(self):
+        self.geodata = get_city_geodata(self.initial_data.get("name", ""))
+
+    def create(self, validated_data):
+        validated_data.update(self.geodata)
+        city = create_city(**validated_data)
+        city.save()
+        return city
+
 
 class CoordinatesSerializer(serializers.Serializer):
     latitude = serializers.DecimalField(
